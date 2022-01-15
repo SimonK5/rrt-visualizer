aa3a870ee33c7937d04f827ef9fa0f2ad59ebf7d
Simon <5222sak@gmail.com>
2022-01-14 23:51:33 -0500

    setup

---
+++ app.js
@@ -0,0 +1,15 @@
+function simToViewX(x){
+  return x + 50;
+}
+
+function simToViewY(y){
+  return y + 50;
+}
+
+function viewToSimX(x){
+  return x - 50;
+}
+
+function viewToSimY(y){
+  return y - 50 + range[0];
+}
